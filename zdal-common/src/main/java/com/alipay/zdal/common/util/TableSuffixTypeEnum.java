a47b6cd49e444cf041fe07f867d112b02fa50aa1
yuanwhy <henrywangwhy@gmail.com>
2017-03-25 16:57:09 +0800

    format  TableSuffixTypeEnum

--- zdal-common/src/main/java/com/alipay/zdal/common/util/TableSuffixTypeEnum.java
+++ zdal-common/src/main/java/com/alipay/zdal/common/util/TableSuffixTypeEnum.java
@@ -5,20 +5,21 @@
 package com.alipay.zdal.common.util;
 
 /**
- * 
  * @author ²®ÑÀ
  * @version $Id: TableSuffixTypeEnum.java, v 0.1 2014-1-6 ÏÂÎç05:23:35 Exp $
  */
 public enum TableSuffixTypeEnum {
-    twoColumnForEachDB("twoColumnForEachDB"), dbIndexForEachDB("dbIndexForEachDB"), groovyTableList(
-                                                                                                    "groovyTableList"), groovyAdjustTableList(
-                                                                                                                                              "groovyAdjustTableList"), groovyThroughAllDBTableList(
-                                                                                                                                                                                                    "groovyThroughAllDBTableList"), throughAllDB(
-                                                                                                                                                                                                                                                 "throughAllDB"), resetForEachDB(
-                                                                                                                                                                                                                                                                                 "resetForEachDB");
+    twoColumnForEachDB("twoColumnForEachDB"),
+    dbIndexForEachDB("dbIndexForEachDB"),
+    groovyTableList("groovyTableList"),
+    groovyAdjustTableList("groovyAdjustTableList"),
+    groovyThroughAllDBTableList("groovyThroughAllDBTableList"),
+    throughAllDB("throughAllDB"),
+    resetForEachDB("resetForEachDB");
+    
     private String value;
 
-    private TableSuffixTypeEnum(String value) {
+    TableSuffixTypeEnum(String value) {
         this.value = value;
     }
 
